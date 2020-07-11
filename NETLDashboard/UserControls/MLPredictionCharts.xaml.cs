fbd416535061242a8b13db4a7530474738cd036f
Jason <jgarv007@fiu.edu>
2020-07-11 16:24:04 -0400

    Inital push for the predictions page. Added user controls to allow for visualization of the models.

---
+++ NETLDashboard/UserControls/MLPredictionCharts.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Controls;
+using LiveCharts;
+using LiveCharts.Wpf;
+
+namespace NETLDashboard.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MLPredictionCharts.xaml
+    /// </summary>
+    public partial class MLPredictionCharts : UserControl
+    {
+        public MLPredictionCharts()
+        {
+            InitializeComponent();
+
+            CorrectValue = new ChartValues<Double> { 60 };
+            MaliciousValue = new ChartValues<Double> { 40 };
+
+            DataContext = this;
+        }
+
+        public ChartValues<Double> CorrectValue { get; set; }
+        public ChartValues<Double> MaliciousValue { get; set; }
+    }
+}
---
+++ NETLDashboard/UserControls/MLResults.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NETLDashboard.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MLResults.xaml
+    /// </summary>
+    public partial class MLResults : UserControl
+    {
+        public MLResults()
+        {
+            InitializeComponent();
+
+            Model.Content = "Model Name";
+            Algorithm.Content = "Algorithm";
+            Accuracy.Content = "Accuracy Score";
+            F1.Content = "F1 Score";
+        }
+
+        //public String ModelName { get; set; }
+        //public String AlgoType { get; set; }
+        //public String AccScore { get; set; }
+        //public String F1Score { get; set; }
+
+    }
+}
