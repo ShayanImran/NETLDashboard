bb7c33c83ceb0310cb59efbd24fcbcaf29a72457
Jason <jgarv007@fiu.edu>
2020-07-13 22:00:25 -0400

    Cleaned up the code in the MachineLearningDashboard user control, added some comments and fixed an issue with the prediction page's algorithm box duplicating entires.

--- NETLDashboard/UserControls/MachineLearningDashboard.xaml.cs
+++ NETLDashboard/UserControls/MachineLearningDashboard.xaml.cs
@@ -13,7 +13,6 @@ namespace NETLDashboard.UserControls
     public partial class MachineLearningDashboard : UserControl
     {
         Db fiu = new Db();
-        List<DDLComponent> componentList;
         List<DDLAlgorithm> algorithmList;
         List<MLPredictionInfo> builtModels;
         List<String> selectedAlgorithms;
@@ -22,7 +21,6 @@ namespace NETLDashboard.UserControls
         public MachineLearningDashboard()
         {
             InitializeComponent();
-            componentList = new List<DDLComponent>();
             algorithmList = new List<DDLAlgorithm>();
             builtModels = new List<MLPredictionInfo>();
             selectedAlgorithms = new List<String>();
@@ -30,11 +28,6 @@ namespace NETLDashboard.UserControls
             //BindDropDown();
             BindDropDownAlgo();
             BindDropDownModels();
-        }
-
-        private void BindDropDown()
-        {
-            ComponentBox.ItemsSource = componentList;
         }
 
         private void AllComponents_CheckedAndUnchecked(object sender, RoutedEventArgs e)
@@ -67,36 +60,12 @@ namespace NETLDashboard.UserControls
                 }
             }
         }
-        private void Component_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            ComponentBox.ItemsSource = componentList.Where(x => x.ComponentName.StartsWith(ComponentBox.Text.Trim()));
-        }
 
         private void ComponentBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             BindListBox();
         }
 
-        private void AddComponentsToList()
-        {
-            DDLComponent thing = new DDLComponent();
-            thing.ComponentName = "Furnace";
-            thing.ComponentId = 2;
-            componentList.Add(thing);
-            thing = new DDLComponent();
-            thing.ComponentName = "Boiler";
-            thing.ComponentId = 1;
-            componentList.Add(thing);
-            thing = new DDLComponent();
-            thing.ComponentName = "Stack";
-            thing.ComponentId = 4;
-            componentList.Add(thing);
-            thing = new DDLComponent();
-            thing.ComponentName = "Turbine";
-            thing.ComponentId = 3;
-            componentList.Add(thing);
-
-        }
 
         private void BindDropDownAlgo()
         {
@@ -106,54 +75,53 @@ namespace NETLDashboard.UserControls
 
         private void BindDropDownModels()
         {
-            fiu.getBuiltModels(builtModels);
-
-            List<String> modelNames = new List<String>();
-            for(int i = 0; i < builtModels.Count; i++)
+            if (builtModels.Count == 0)
             {
-                modelNames.Add(builtModels[i].ModelName);
+                fiu.getBuiltModels(builtModels);
+
+                List<String> modelNames = new List<String>();
+                for (int i = 0; i < builtModels.Count; i++)
+                {
+                    modelNames.Add(builtModels[i].ModelName);
+                }
+                PredictionsModelName.ItemsSource = modelNames;
             }
-            PredictionsModelName.ItemsSource = modelNames; 
+            else
+            {
+                builtModels.Clear();
+
+                fiu.getBuiltModels(builtModels);
+
+                List<String> modelNames = new List<String>();
+                for (int i = 0; i < builtModels.Count; i++)
+                {
+                    modelNames.Add(builtModels[i].ModelName);
+                }
+                PredictionsModelName.ItemsSource = modelNames;
+            }
         }
 
         private void BindPredictionAlgos(String currentModel)
         {
-            String[] temp = new String[3];
+            List<MLPredictionInfo> temp = new List<MLPredictionInfo>();
             for(int i = 0; i < builtModels.Count; i++)
             {
-                if(currentModel.Equals(builtModels[i].ModelName))
+                if(currentModel.Equals(builtModels[i].ModelName)) // Searches for the specific model name to pull the informoation from.
                 {
-                    //MessageBox.Show(builtModels[i].ModelName.ToString());
-                    String[] algos = builtModels[i].ModelAlgos.Split(',');
-                    
-                    for(int j = 0; j < algos.Length; j++)
+                    String[] algos = builtModels[i].ModelAlgos.Split(','); //Splits the string thats stored in the coloumn of the db entry into separate algorithm names
+                    for (int j = 0; j < algos.Length; j++)
                     {
-                        MLPredictionInfo val = builtModels[i];
-                        val.ModelAlgos = algos[j];
-                        temp[j] = val.ModelAlgos;
-                       // MessageBox.Show(temp[j].ToString());
-                        //PredictionsAlgorithmsBox.Items.Insert(j, tempObject[j].ModelAlgos.ToString());
+                        temp.Add(new MLPredictionInfo()); //Populating the list with an empty object. Needs to be done this way to avoid binding errors.
+                        temp[j].ModelAlgos = algos[j]; //Setting the empty object's ModelAlgo attribute to the algorithm name
                     }
                 }
 
             }
-            PredictionsAlgorithmsBox.ItemsSource = temp;
+            PredictionsAlgorithmsBox.ItemsSource = temp;//Passes the list of objects to the box.
 
         }
 
         // Makes a call to the database to retrieve all the machine learning algorithm names and adds to dropdown list
-
-        private void AlgorithmBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            AlgorithmsBox.ItemsSource = algorithmList.Where(x => x.AlgorithmName.StartsWith(AlgorithmsBox.Text.Trim()));
-        }
-
-        private void AlgorithmBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-
         private void Run_Button_Click(object sender, RoutedEventArgs e)
         {
             // Removing the comma at the end of the string
@@ -186,38 +154,10 @@ namespace NETLDashboard.UserControls
 
         }
 
-        private void checkModels_Checked(object sender, RoutedEventArgs e)
-        {
-
-        }
 
         private void PredictionsModelName_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             BindPredictionAlgos(PredictionsModelName.SelectedValue.ToString());
-        }
-
-        private void PredictionsAlgorithmsBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void PredictionsAlgorithmsBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            
-        }
-    }
-
-    public class DDLComponent
-    {
-        public int ComponentId
-        {
-            get;
-            set;
-        }
-        public string ComponentName
-        {
-            get;
-            set;
         }
 
     }
@@ -250,9 +190,6 @@ namespace NETLDashboard.UserControls
         public String ModelComponent { get; set; }
 
         public String ModelAlgos { get; set; }
-
-        public Boolean CheckedStatus { get; set; }
-
 
     }
 
